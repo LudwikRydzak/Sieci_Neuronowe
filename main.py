7eae2e772fe3765f973ac0d9231113faaf66ba97
Ludwik Rydzak <ludwik.rydzak@gmail.com>
2020-10-06 21:53:15 +0200

    poprawione działanie, na ten moment perceptron uczy się pilnie i nie przestaje się uczyć wcale.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from perceptron import *
 
-perceptron = Perceptron(2, 0.2)
+perceptron = Perceptron(2, 0.002)
 learning_set = [[1,1,1],[0,0,1],[0,1,0],[0,0,0]]
 perceptron.learn(learning_set)
--- perceptron.py
+++ perceptron.py
@@ -1,23 +1,25 @@
 import random
-
+import numpy as np
 
 class Perceptron:
     def __init__(self, _entry_count, _learning_factor):
         self.entry_values = []
         self.entry_weights = []
         self.entry_values.append(1)
+        self.bias = 5
         self.entry_weights.append(random.random() / 10)
+
         for i in range(_entry_count):
             self.entry_values.append(0)
             self.entry_weights.append(random.random() / 10)
         self.learning_factor = _learning_factor
 
     def bipolar_function(self, _sum):
-        return (1 if _sum > self.bias else -1)
+        return 1 if _sum > 0 else -1
 
     def entry_function(self, _entry_values):
-        for i in range(len(self.entry_values)):
-            self.entry_values[i] = 1 if _entry_values[i] > 0 else -1
+        for i in range(len(_entry_values)):
+            self.entry_values[i+1] = 1 if _entry_values[i] > 0 else -1
 
     def sum(self):
         sum = 0;
@@ -25,26 +27,29 @@ class Perceptron:
             sum += self.entry_values[i] * self.entry_weights[i]
         return sum
 
-    def weight_change(_label, _entry_value, _final_value):
+    def weight_change(self, _label, _entry_value, _final_value):
         return (_label - _final_value) * _entry_value
 
     def learn(self, _learning_set):
         epoch = 1
+        display_epoch =''
         is_learning_error = True
-        while (is_learning_error):
+        while (is_learning_error and epoch<20000):
+            is_learning_error = False
+            display_epoch = display_epoch+'.'
             for i in range(len(_learning_set)):
-                label, entry_values = _learning_set[0], _learning_set[1:]
-                self.entry_function(entry_values)
+                set = _learning_set[i]
+                label, entry_values_set = set[0], set[1:]
+                self.entry_function(entry_values_set)
                 sum = self.sum()
                 output = self.bipolar_function(sum)
-                for j in range(entry_values):
-                    change = self.weight_change(label, entry_values[j], output)
+                for j in range(len(self.entry_values)):
+                    change = self.weight_change(label, self.entry_values[j], output)
                     if (change != 0):
                         self.entry_weights[j] = self.entry_weights[j] + self.learning_factor * change
                         is_learning_error = True
-                    else:
-                        is_learning_error = False
+            print(epoch)
             epoch += 1
-            print('.')
-        print(epoch)
+
+        print(display_epoch)
         print(self.entry_weights)
