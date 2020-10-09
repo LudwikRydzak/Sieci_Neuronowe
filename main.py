337cac9e6af5d85300c0647aa781332b2bff18e8
Ludwik Rydzak <ludwik.rydzak@gmail.com>
2020-10-09 10:20:25 +0200

    Perceptron uczy się bardzo dobrze ;) W końcu.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from perceptron import *
 from generator import *
 perceptron = Perceptron(2, 0.0001)
-learning_set = [[1,1,1],[-1,0,1],[-1,1,0],[-1,0,0]]
-learning_set = generate(5)
+#learning_set = [[1,1,1],[-1,0,1],[-1,1,0],[-1,0,0]]
+learning_set = generate(10)
 perceptron.learn(learning_set)
+print(perceptron.prediction([0,0]))
+print(perceptron.prediction([0,1]))
+print(perceptron.prediction([1,0]))
 print(perceptron.prediction([1,1]))
--- venv/generator.py
+++ venv/generator.py
@@ -4,9 +4,9 @@ def generate(_amount):
     for i in range(_amount):
         a = random.random()/100
         b = random.random()/100
-        result.append([-1,a,b])
-        result.append([-1,a+b,a-b])
-        result.append([-1,b,a])
+        result.append([-1,a-1,b-1])
+        result.append([-1,(a+b)-1,1-a])
+        result.append([-1,1-b,a-1])
         result.append([1,1-a,1-b])
     return result
 
